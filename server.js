327f29407aed994210c57dd037bbf7f271e8285e
Ilpupi00 <168643943+Ilpupi00@users.noreply.github.com>
2025-05-30 18:07:09 +0000

    aggiunta di file dumb

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express=require('express');
+const port=3000;
+
+const app=express();
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(res,req)=>{
+    res.sendFile (__dirname + '/index.html');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
