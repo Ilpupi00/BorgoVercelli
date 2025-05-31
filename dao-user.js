edbb24fe1d821419049124441790fe70402a77df
Ilpupi00 <168643943+Ilpupi00@users.noreply.github.com>
2025-05-31 18:15:25 +0000

    dao-user

--- dao-user.js
+++ dao-user.js
@@ -6,9 +6,20 @@ const bcrypt=require('bcrypt');
 
 exports.createUser=function(user){
     return new Promise((resolve,reject)=>{
-        const sql = 'INSERT INTO UTENTI (id,nome'
-    })
-
+        const sql = 'INSERT INTO UTENTI (id,nome, cognome, email, password,telefono) VALUES (?,?,?,?,?)';
+        bcrypt.hash(user.password,10).then((hash)=>{
+            sqlite.run(sql, [user.id, user.nome, user.cognome, user.email, hash, user.telefono])
+            .then(() => {
+                resolve({ message: 'User created successfully' });
+            })
+            .catch((err) => {
+                reject({ error: 'Error creating user: ' + err.message });
+            });
+        }
+        ).catch((err) => {
+            reject({ error: 'Error hashing password: ' + err.message });
+        });
+    });
 }
 
 
